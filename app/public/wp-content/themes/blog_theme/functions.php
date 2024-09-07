2d86cd05900ea678ab0b36a22bfc7d39ceaea576
nikogenix <nikogenix@gmail.com>
2024-09-07 21:17:44 +0300

    Update functions

--- app/public/wp-content/themes/blog_theme/functions.php
+++ app/public/wp-content/themes/blog_theme/functions.php
@@ -26,6 +26,7 @@ function load_features()
     add_theme_support("title-tag");
     add_theme_support("menus");
     add_theme_support('post-thumbnails');
+    add_theme_support('widgets');
 
 }
 add_action("after_setup_theme", "load_features");
@@ -41,3 +42,50 @@ register_nav_menus(
 
 add_image_size('blog-large', 800, 400, true);
 add_image_size('blog-small', 300, 200, true);
+
+
+function load_sidebars()
+{
+
+    register_sidebar([
+        'name' => 'Post Sidebar',
+        'id' => 'post-sidebar',
+        'before_widget' => '<div id="%1$s" class="%2$s post-sidebar">',
+        'after_widget' => '</div>',
+    ]);
+
+}
+add_action('widgets_init', 'load_sidebars');
+
+function add_book_post_type()
+{
+
+    register_post_type('books', [
+        'labels' => [
+            'name' => 'Books',
+            'singular_name' => 'Book',
+        ],
+        'public' => true,
+        'has_archive' => true,
+        'supports' => ['title', 'editor', 'thumbnail'],
+        'hierarchical' => true,
+        'menu_icon' => 'dashicons-book-alt',
+    ]);
+
+}
+add_action('init', 'add_book_post_type');
+
+function add_genre_taxonomy()
+{
+
+    register_taxonomy('genre', ['books'], [
+        'labels' => [
+            'name' => 'Genres',
+            'singular_name' => 'Genre',
+        ],
+        'public' => true,
+        'hierarchical' => false,
+    ]);
+
+}
+add_action('init', 'add_genre_taxonomy');
