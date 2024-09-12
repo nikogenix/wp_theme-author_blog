39a735d9e12b21c2a1a5c97a172f4ec6274fbcce
nikogenix <nikogenix@gmail.com>
2024-09-12 21:31:43 +0300

    Replacing emoji with unicode

--- app/public/wp-content/themes/blog_theme/searchform.php
+++ app/public/wp-content/themes/blog_theme/searchform.php
@@ -1,6 +1,6 @@
 <form action="/" method="get">
 
     <input type="text" name="s" id="search" value="<?php the_search_query(); ?>" required>
-    <button type="submit">🔎</button>
+    <button type="submit">&#x1f50d;&#xFE0E;</button>
 
 </form>
